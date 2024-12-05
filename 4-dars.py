c66334233bd0ba3a3cd017e4f94ef71eb83b57f8
Avazbek <polatovavazbek36@gmail.com>
2024-12-05 15:16:23 +0500

    Employee clasini yozdik

--- 4-dars.py
+++ 4-dars.py
@@ -1,1 +1,13 @@
 print("Hello")
+
+def calculate(num1, num2):
+    return num1 + num2  
+
+result = calculate(3, 5)
+print(result)
+
+class Employee:
+    def __init__(self, name, salary,last_name):
+        self.name = name
+        self.salary = salary
+        self.last_name = last_name
